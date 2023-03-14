d910502d1c63cca16d8c5319517127f966fdad6d
Felix <hao.an@hp.com>
2023-03-14 09:51:55 +0800

    loop

--- compound_type/src/main.rs
+++ compound_type/src/main.rs
@@ -127,19 +127,34 @@
 //     println!("The value of number is: {}", number);
 // }
 
-fn main() {
-    let n = 6;
+// fn main() {
+//     let n = 6;
 
-    if n % 4 == 0 {
-        println!("number is divisible by 4");
-    } else if n % 3 == 0 {
-        println!("number is divisible by 3");
-    } else if n % 2 == 0 {
-        println!("number is divisible by 2");
-    } else {
-        println!("number is not divisible by 4, 3, or 2");
-    }
+//     if n % 4 == 0 {
+//         println!("number is divisible by 4");
+//     } else if n % 3 == 0 {
+//         println!("number is divisible by 3");
+//     } else if n % 2 == 0 {
+//         println!("number is divisible by 2");
+//     } else {
+//         println!("number is not divisible by 4, 3, or 2");
+//     }
+// }
+
+fn main() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {}", result);
 }
+
 
 
 
