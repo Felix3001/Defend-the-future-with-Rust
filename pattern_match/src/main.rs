ff1086e544e3d9467fb4897b4426467de281ba39
Felix <hao.an@hp.com>
2023-03-14 09:59:41 +0800

    pattern_match

---
+++ pattern_match/src/main.rs
@@ -0,0 +1,20 @@
+
+
+#[warn(dead_code)]
+enum Direction {
+    East,
+    West,
+    North,
+    South,
+}
+
+fn main() {
+    let dire = Direction::South;
+    match dire {
+        Direction::East => println!("East"),
+        Direction::North | Direction::South => {
+            println!("South or North");
+        },
+        _ => println!("West"),
+    };
+}
