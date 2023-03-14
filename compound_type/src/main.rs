fd5689fbb8cfdcce35ac1e0b25e45d8c54531eba
Felix <hao.an@hp.com>
2023-03-14 09:49:41 +0800

    if/else

--- compound_type/src/main.rs
+++ compound_type/src/main.rs
@@ -107,11 +107,39 @@
 // }
 
 
+// fn main() {
+//     let mut i = 5;
+//     while i >= 1 {
+//         println!("{}", i);
+//         i = i - 1;
+//         println!("{}", i);
+//     }
+// }
+
+// fn main() {
+//     let condition = true;
+//     let number = if condition {
+//         5
+//     } else {
+//         6
+//     };
+
+//     println!("The value of number is: {}", number);
+// }
+
 fn main() {
-    let mut i = 5;
-    while i >= 1 {
-        println!("{}", i);
-        i = i - 1;
-        println!("{}", i);
+    let n = 6;
+
+    if n % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if n % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if n % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
     }
 }
+
+
+
